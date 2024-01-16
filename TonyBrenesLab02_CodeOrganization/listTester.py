74885ffc9fb78a264d5d54f3a3085b9a9ec998a5
brenes1ad <adbrenes05@gmail.com>
2024-01-16 15:44:58 -0500

    Testing push for lab2 I dont think it will work

---
+++ TonyBrenesLab02_CodeOrganization/listTester.py
@@ -0,0 +1,44 @@
+"""
+Main.py -- a testbed for different list initializations that we
+instrument with our Stopwatch class
+
+T. Brenes
+CSC121 W24
+1/16/24
+Lab2
+"""
+import stopwatch
+
+
+
+"""
+list_tests() -- try three different ways to make a list of 10_000_000 zeros
+"""
+def mainListTests(n):
+    timerSec = stopwatch.Stopwatch()
+    timerNS = stopwatch.Stopwatch()
+
+    # Method 1 -- using a for-loops
+    nums = []
+    timerSec.start()
+    timerNS.startNS()
+    for i in range(n):
+        nums.append(0)
+    elapsedSec = timerSec.stop()
+    elapsedNS = timerNS.stop()
+    print("It took", elapsedSec, "seconds to create list of 10 million zeros using method 1")
+    print("It took %d nanoseconds to create list of 10 million zeros using method 1" % elapsedNS)
+    print()
+
+    # Method 2 -- using a list-comprehension
+    nums = [0 for i in range(n)]
+
+    # Method 3 -- built in lists-multiplier
+    timerNS.startNS()
+    nums = [0] * n
+    elapsed = timerNS.stopNS()
+    print("It took %d nanoseconds to create list of 10 million zeros using method 3" % elapsed)
+
+
+if __name__ == "__main__":
+    mainListTests(10_000_000)
---
+++ TonyBrenesLab02_CodeOrganization/stopwatch.py
@@ -0,0 +1,69 @@
+"""
+stopwatch.py -- timer class to give simple instrumenting capabilities to time
+using seconds or nanoseconds
+
+T. Brenes
+CS121 W24
+1/16/24
+Lab2
+"""
+import time
+
+class Stopwatch:
+
+    def __init__(self):
+        self.start_time = 0
+
+    def start(self):
+        self.start_time = time.perf_counter()
+
+    def stop(self):
+        """
+        get elapsed time in fractional seconds and reset to zero
+        :return: (float) seconds
+        """
+        elapsed = time.perf_counter() - self.start_time
+        self.start_time = 0
+        return elapsed
+    def split(self):
+        """
+        get elapsed time as above but don't reset time
+        :return: (float) seconds
+        """
+        return time.perf_counter() - self.start_time
+
+    def startNS(self):
+        self.start_timeNS = time.perf_counter_ns()
+
+    def stopNS(self):
+        st = self.start_timeNS
+        curr = time.perf_counter_ns()
+        print("time = %d and starttime = %d" %(st, curr))
+        print("and their difference is %d" %(curr - st))
+        elapsedNS = time.perf_counter_ns() - self.start_timeNS
+        self.start_timeNS = 0
+        return elapsedNS
+
+    def splitNS(self):
+        return time.perf_counter_ns() - self.start_timeNS
+
+def test():
+    print("Stopwatch tests")
+    timer = Stopwatch()
+    timerNS = Stopwatch()
+    timer.start()
+    timerNS.startNS()
+    for i in range(10_000_000):
+        pass
+    elapsed = timer.stop()
+    elapsedNS = timerNS.stopNS()
+    print("elapsed time =", elapsed)
+    print("NS TIme", elapsedNS)
+
+
+
+
+if __name__ == "__main__":
+    test()
+else:
+    print("stopwatch's __name__ value: ", __name__)
