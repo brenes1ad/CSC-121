3d61c0548a503259ebb0486c7f786c5c998b778b
brenes1ad <adbrenes05@gmail.com>
2024-02-15 11:13:48 -0500

    Lab 06

--- TonyBrenesLab06_EuclidAndSorting/timeTrials.py
+++ TonyBrenesLab06_EuclidAndSorting/timeTrials.py
@@ -2,10 +2,22 @@ import BetterStopwatch
 from random import randint
 from math import log2
 
+def gcd(a, b):
+    count = 0
+    if a < 0:
+        a = -a
+    if b < 0:
+        b = -b
+    while b != 0:
+        a, b = b, a % b
+        count += 1
+    return count
+
 timer = BetterStopwatch.BetterStopwatch()
 k = 500
 averageTimes = []
 checkLog = []
+print("List sorting:")
 for n in range(12):
     totalTime = 0
     for i in range(20):
@@ -19,7 +31,24 @@ for n in range(12):
     checkLog.append(averageTime / (k*log2(k)))
     k *= 2
 print(averageTimes)
-print(checkLog)
+print(checkLog, "\n")
+
+k = 10
+averageCounts = []
+checkFunction = []
+print("GCD:")
+for n in range(12):
+    totalCounts = 0
+    for i in range(20):
+        totalCounts += gcd(randint(0, k), randint(0, k))
+    averageCount = totalCounts // 20
+    averageCounts.append(averageCount)
+    checkFunction.append(averageCount / log2(k))
+    k *= 10
+print(averageCounts)
+print(checkFunction)
+
+
 
 
 
