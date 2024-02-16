b1d7281bc63d83deb6f925f2fe2128768cb46024
brenes1ad <adbrenes05@gmail.com>
2024-02-16 13:18:04 -0500

    Lab 06 Ready to turn-in

--- TonyBrenesLab06_EuclidAndSorting/timeTrials.py
+++ TonyBrenesLab06_EuclidAndSorting/timeTrials.py
@@ -18,6 +18,7 @@ k = 500
 averageTimes = []
 checkLog = []
 print("List sorting:")
+print("%7s %10s %20s" % ("k", "averageTime", "averageTime / (k*log2(k))"))
 for n in range(12):
     totalTime = 0
     for i in range(20):
@@ -29,14 +30,15 @@ for n in range(12):
     averageTime = totalTime / 20
     averageTimes.append(averageTime)
     checkLog.append(averageTime / (k*log2(k)))
+    print("%7s %10s %20s" % (k, averageTime, averageTime / (k*log2(k))))
     k *= 2
-print(averageTimes)
-print(checkLog, "\n")
+
 
 k = 10
 averageCounts = []
 checkFunction = []
 print("GCD:")
+print("%15s %10s %15s" % ("k", "averageCount", "(averageCount / log2(k))"))
 for n in range(12):
     totalCounts = 0
     for i in range(20):
@@ -44,14 +46,8 @@ for n in range(12):
     averageCount = totalCounts // 20
     averageCounts.append(averageCount)
     checkFunction.append(averageCount / log2(k))
+    print("%15d %10d %15f" % (k, averageCount, (averageCount / log2(k))))
     k *= 10
-print(averageCounts)
-print(checkFunction)
-
-
-
-
-
 
 
 
