65f7885788f4ebd0508fd3ac5b7a74a772723e5d
brenes1ad <adbrenes05@gmail.com>
2024-03-21 14:14:28 -0400

    Lab 10 almost working

--- TonyBrenesLab10_Queueing/queueTest.py
+++ TonyBrenesLab10_Queueing/queueTest.py
@@ -22,41 +22,57 @@ with open("web2.txt", "r", encoding="utf8") as f:
 
 # Create and test our ArrayQueue
 arrQ = ArrayQueue()
-for i in range(10):
-    print("enqueueing %d" % i)
-    arrQ.enqueue(i)
-
-print("arrQ has %d elements" %len(arrQ))
-
-try:
-    while True:
-        print("removing %d" % arrQ.dequeue())
-except:
-    print("queue now empty")
-print("size of queue = %d" %len(arrQ))
+# for i in range(10):
+#     print("enqueueing %d" % i)
+#     arrQ.enqueue(i)
+#
+# print("arrQ has %d elements" %len(arrQ))
+#
+# try:
+#     while True:
+#         print("removing %d" % arrQ.dequeue())
+# except:
+#     print("queue now empty")
+# print("size of queue = %d" %len(arrQ))
 
 print("")
 
-wordQ = ArrayQueue()
-for _ in range(10):
-    word = choice(wordList)
-    print(f"enqueueing {word}")
-    wordQ.enqueue(word)
-print("")
-
-try:
-    while True:
-        print(f"removing {wordQ.dequeue()}" )
-except:
-    print("queue now empty")
-print("size of queue = %d" %len(arrQ))
+# wordQ = ArrayQueue()
+# for _ in range(10):
+#     word = choice(wordList)
+#     print(f"enqueueing {word}")
+#     wordQ.enqueue(word)
+# print("")
+#
+# try:
+#     while True:
+#         print(f"removing {wordQ.dequeue()}" )
+# except:
+#     print("queue now empty")
+# print("size of queue = %d" %len(arrQ))
 
 
 print("\n","TextArrayPriorityQueue")
 # Create and test our TextArrayPriorityQueue
 
 pq = TextPQ()
-print(f"len pq = {len(pq)}")
-pq.enqueue(wordList[5])
-print(f"len pq = {len(pq)}")
-print(f"popping! {pq.dequeue()}")
+# pq.enqueue(wordList[20000])
+# pq.enqueue(wordList[19000])
+# pq.enqueue(wordList[5])
+#
+# print(f"len pq = {len(pq)}")
+# print(f"popping! {pq.dequeue()}")
+# print(f"popping! {pq.dequeue()}")
+# print(f"popping! {pq.dequeue()}")
+
+for _ in range(30):
+    word = choice(wordList)
+    print(f"enqueueing {word}")
+    pq.enqueue(word)
+print("")
+
+try:
+    while True:
+        print(f"removing {pq.dequeue()}")
+except:
+    print("queue now empty")
