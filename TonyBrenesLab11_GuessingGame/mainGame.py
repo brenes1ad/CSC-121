68931e1bad3098061242d678634411174ac2783e
brenes1ad <adbrenes05@gmail.com>
2024-04-10 15:51:42 -0400

    Lab 11 Finished

--- TonyBrenesLab11_GuessingGame/mainGame.py
+++ TonyBrenesLab11_GuessingGame/mainGame.py
@@ -5,6 +5,8 @@ sort of game. Tree will be self-updating polymorphic binary tree.
 Tony Brenes
 CSC121 W24
 4/2/2024
+
+All code was used to assist in the creation of David Amo's code as well
 """
 
 from queryNode import QueryNode
@@ -16,9 +18,15 @@ import pickle
 
 def main():
     """run the program: load tree, play rounds, save updated tree"""
+    """
+    for every pop-up except the first introduction and continue, pressing x-button will bring back the same prompt to avoid empty 
+    tree Nodes, for intro pop-up, x-button will quit out of the game, pressing x-button on continue popup serves as pressing No
+    """
 
     # greet user, ask if wants to play a game: ready?
-    sg.popup("Hey, lets play a fun game!", title="Guess the Assassin's Creed Character Game")
+    start = sg.popup("Hey, lets play a fun game!", title="Guess the Assassin's Creed Character Game")
+    if start is None:
+        return
 
 
     # load data from .gametree.pkl file if exists, else create base case
@@ -32,18 +40,20 @@ def main():
 
 
     #enter game loop
-    keepPlaying = True
-    while keepPlaying:
+    while True:
         gameTree = gameTree.play()
         if sg.popup_yes_no("Would you like to continue playing?", title="Continue?") != "Yes":
-            keepPlaying = False
+            break
 
 
 
 
 
     #ask if want to save? if yes, pickle current tree to new default file
-    wantToSave = sg.popup_yes_no("Do you want to save this tree of questions and answers?", title="save")
+    while True:
+        wantToSave = sg.popup_yes_no("Do you want to save this tree of questions and answers?", title="Save?")
+        if wantToSave is not None:
+            break
     if wantToSave == "Yes":
         with open("treeout.pkl", "wb") as f:
             pickle.dump(gameTree, f)
